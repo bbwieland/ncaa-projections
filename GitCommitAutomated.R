7a381f8fd9a9b7c7d43f347aa762a5a77f4e53c1
bbwieland <benbowenwieland@gmail.com>
2023-01-14 10:42:42 -0600

    test

--- GitCommitAutomated.R
+++ GitCommitAutomated.R
@@ -1,6 +1,8 @@
 library(git2r)
 
-add(repo = getwd(), path = "GitCommitAutomated.R")
+path = "/Users/ben/Desktop/ncaa-projections/data/"
+
+add(repo = getwd(), path = s.)
 
 commit(repo = getwd(), message = paste0("Data update at: ", Sys.time()))
 
